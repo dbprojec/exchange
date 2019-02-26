b921829da2171af434e4199eed9917efe57b4733
Henok Million <henokmillionmekuria@gmail.com>
2019-02-26 11:58:54 +0300

    about to do graphs

--- cryptoexchange/src/graph/graph.js
+++ cryptoexchange/src/graph/graph.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react'
 import {HttpUtil} from '../utils/http.util.ts'
 import Typography from '@material-ui/core/Typography';
-var LineChart = require("react-chartjs").Line;
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
 
-export default class Graph extends Component {
+var LineChart = require("react-chartjs").Line;
+const styles = {
+  card: {
+    maxWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+};
+
+class Graph extends Component {
   
   httpUtil = HttpUtil.getInstance()
   state = {
@@ -12,34 +34,61 @@ export default class Graph extends Component {
       '/btc': 'Bitcoin',
       '/eth': 'Etherium'
     },
-    url: ''
+    url: '',
+    loaded: false,
+    graphData: [],
+    urlSet: false
+
   }
 
   constructor(props) {
     super(props)
     this.fetchData = this.fetchData.bind(this)
-
   }
-  componentWillMount() {
-    this.setState({url: this.props.location.pathname})
-    if (this.state.url === '/btc') {
-      this.fetchData(HttpUtil.ETHER_URL)
+  componentWillUnmount() {
+    this.setState({loaded: false})
+  }
+  componentDidMount() {
+    this.setState({url: this.props.location.pathname, urlSet: true})
+    if (this.props.location.pathname.indexOf('/btc') > -1) {
+      this.fetchData(HttpUtil.BTC_URL, HttpUtil.BTC_GRAPH_URL)
     } else {
-      this.fetchData(HttpUtil.ETHER_URL)
+      this.fetchData(HttpUtil.ETH_URL, HttpUtil.BTC_GRAPH_URL)
     }
   }
 
-  fetchData(url) {
-    this.httpUtil.fetchData(url).then(data => console.log(data))
+  fetchData(url, graph_url) {
+    this.httpUtil.fetchData(url).then(data => this.setState({data: data}))
+    this.httpUtil.fetchData(graph_url).then(data => this.setState({graphData: data.values, loaded: true}))
   }
 
   render() {
+    const {classes} = this.props;
     // const url = "https://api.blockchain.info/charts/transactions-per-second?timespan=5weeks&rollingAverage=8hours&format=json"
     return (
-      <div>
+      !this.state.loaded?(<div></div>):(
+        <div>
         <Typography className="header" variant="h2">{this.state.titles[this.state.url]}</Typography>
-        <LineChart data={this.state.data} options={this.props} width="600" height="250"/>
+        <LineChart data={this.state.graphData} options={this.props} width="600" height="250"/>
+        <Card className={classes.card}>
+          <CardContent>
+            <Typography gutterBottom>
+              {this.state.titles[this.state.url]}
+            </Typography>
+            <Typography variant="h5" component="h2">
+            </Typography>
+            <Typography color="textSecondary">
+              ${this.state.data.ticker.price}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Details</Button>
+          </CardActions>
+        </Card>
       </div>
+      )
     )
   }
 }
+
+export default withStyles(styles)(Graph);
--- cryptoexchange/src/utils/http.util.ts
+++ cryptoexchange/src/utils/http.util.ts
@@ -5,8 +5,9 @@ export class HttpUtil {
     static httpUtil: HttpUtil;
     static API_KEY = 'STJXS1XCIR28VUNTG1CCKAQ2BQI6K5V9FG';
     static COIN_API_KEY = '856D86E9-4CF0-473D-96E8-E7F92A30EE90';
-    public static ETHER_URL = `https://api.etherscan.io/api?module=stats&action=ethprice&apikey=${HttpUtil.API_KEY}`;
-    public static BTC_URL =  'https://rest.coinapi.io/v1/exchangerate/BTC';
+    public static ETH_URL = 'https://api.cryptonator.com/api/ticker/eth-usd';
+    public static BTC_URL =  'https://api.cryptonator.com/api/ticker/btc-usd';
+    public static BTC_GRAPH_URL = 'https://api.blockchain.info/charts/transactions-per-second?timespan=5weeks&rollingAverage=8hours&format=json'
     public static getInstance(): HttpUtil {
         if (this.httpUtil != null) {
             return this.httpUtil
@@ -21,21 +22,19 @@ export class HttpUtil {
 
     fetchData(url: string) {
         if (Object.keys(this._cache).indexOf(url) > -1) {
-            console.log(this._cache[url])
+            console.log(this._cache[url].data)
             return new Promise((succeed, fail) => {
-                succeed({data: this._cache[url].data})
+                succeed({...this._cache[url].data})
             })
         } else {
             // alert("returning from network")
-            return fetch(url, {
-                method: 'GET',
+            return axios.get(url, {
                 headers: {
-                    "content-type": "application/json; charset=utf-8",
-                    "X-CoinAPI-Key": HttpUtil.COIN_API_KEY
+                    "content-type": "application/json; charset=utf-8"
                 }
             }).then((res: any) => {
                 this._cache[url] = res
-                return res
+                return res.data
             })
         }
     }
